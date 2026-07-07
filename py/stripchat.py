e40790dde91435166dac08579002b7b3c367a582
FGBLH <pengli08211@gmail.com>
2026-07-08 01:14:00 +0800

    Update stripchat.py

--- py/stripchat.py
+++ py/stripchat.py
@@ -98,7 +98,7 @@ class Spider(Spider):
         pass
 
     def playerContent(self, flag, id, vipFlags):
-        domain = f"https://edge-hls.doppiocdn.net/hls/{id}/master/{id}_auto.m3u8?playlistType=lowLatency"
+        domain = f"https://edge-hls.growcdnssedge.com/hls/{id}/master/{id}_auto.m3u8"
         rsp = requests.get(domain, headers=self.headers).text
         lines = rsp.strip().split('\n')
         psch = ''
